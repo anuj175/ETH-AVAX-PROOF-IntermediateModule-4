4f3f69b32413a603b26e426093d314edf2fbd3c7
anuj175 <121374679+anuj175@users.noreply.github.com>
2023-08-09 00:51:39 +0530

    Update module4.sol

--- module4.sol
+++ module4.sol
@@ -2,51 +2,66 @@
 pragma solidity ^0.8;
 
 contract DegenToken {
-    string public name = "Degen"; // Token name
-    string public symbol = "DGN"; // Token symbol
-    uint8 public decimals = 18; // Token decimals, similar to most ERC-20 tokens
-    uint256 public totalSupply; // Total supply of tokens
+    string public name = "Degen";
+    string public symbol = "DGN";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
 
-    mapping(address => uint256) public balanceOf; // Mapping to store the balance of each address
-    mapping(address => mapping(address => uint256)) public allowance; // Mapping to store the approved allowance for each address
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
-    event Transfer(address indexed from, address indexed to, uint256 value); // Event emitted on token transfer
-    event Approval(address indexed owner, address indexed spender, uint256 value); // Event emitted on approval of spending
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Redeem(address indexed user, uint256 value, string prize);
 
     constructor(uint256 initialSupply) {
-        totalSupply = initialSupply * 10**uint256(decimals); // Set the total supply of tokens
-        balanceOf[msg.sender] = totalSupply; // Assign the total supply to the contract creator's balance
+        totalSupply = initialSupply * 10**uint256(decimals);
+        balanceOf[msg.sender] = totalSupply;
     }
 
     function transfer(address to, uint256 value) external returns (bool) {
-        require(to != address(0), "Invalid address"); // Check if the destination address is valid
-        require(value <= balanceOf[msg.sender], "Insufficient balance"); // Check if the sender has enough balance
+        require(to != address(0), "Invalid address");
+        require(value <= balanceOf[msg.sender], "Insufficient balance");
 
-        balanceOf[msg.sender] -= value; // Deduct the amount from the sender's balance
-        balanceOf[to] += value; // Add the amount to the receiver's balance
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
 
-        emit Transfer(msg.sender, to, value); // Emit a transfer event
+        emit Transfer(msg.sender, to, value);
         return true;
     }
 
     function approve(address spender, uint256 value) external returns (bool) {
-        allowance[msg.sender][spender] = value; // Approve the spender to spend the specified amount
+        allowance[msg.sender][spender] = value;
 
-        emit Approval(msg.sender, spender, value); // Emit an approval event
+        emit Approval(msg.sender, spender, value);
         return true;
     }
 
     function transferFrom(address from, address to, uint256 value) external returns (bool) {
-        require(from != address(0), "Invalid address"); // Check if the source address is valid
-        require(to != address(0), "Invalid address"); // Check if the destination address is valid
-        require(value <= balanceOf[from], "Insufficient balance"); // Check if the source has enough balance
-        require(value <= allowance[from][msg.sender], "Allowance exceeded"); // Check if the spender is allowed to spend the specified amount
+        require(from != address(0), "Invalid address");
+        require(to != address(0), "Invalid address");
+        require(value <= balanceOf[from], "Insufficient balance");
+        require(value <= allowance[from][msg.sender], "Allowance exceeded");
 
-        balanceOf[from] -= value; // Deduct the amount from the source's balance
-        balanceOf[to] += value; // Add the amount to the receiver's balance
-        allowance[from][msg.sender] -= value; // Reduce the spender's allowance by the spent amount
+        balanceOf[from] -= value;
+        balanceOf[to] += value;
+        allowance[from][msg.sender] -= value;
 
-        emit Transfer(from, to, value); // Emit a transfer event
+        emit Transfer(from, to, value);
+        return true;
+    }
+
+    function redeem(string memory prize) external returns (bool) {
+        require(bytes(prize).length > 0, "Prize cannot be empty");
+        uint256 redemptionCost = 100 * 10**uint256(decimals);
+
+        require(balanceOf[msg.sender] >= redemptionCost, "Insufficient balance for redemption");
+
+        balanceOf[msg.sender] -= redemptionCost;
+
+        emit Redeem(msg.sender, redemptionCost, prize);
+
         return true;
     }
 }
+
